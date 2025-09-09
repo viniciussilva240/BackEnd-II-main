6f072658f7ee20575f7c6666c58e42a6215abea8
viniciussilva240 <vinicius.costa@estudante.ifro.edu.br>
2025-09-09 16:51:41 -0400

    first commit

---
+++ apiUF/servicos/servico.js
@@ -0,0 +1,18 @@
+import  colecaoUf  from '../dados/dados.js';
+
+export const buscarUfs = () => {
+  return colecaoUf;
+}
+
+export const buscarUfsPorNome = (nomeUf) => {
+  return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
+};
+
+export const buscarUfPorId = (id) => {
+  const idUF = parseInt(id);
+  return colecaoUf.find(uf => uf.id === idUF);
+}
+
+const buscarUfPorsigla = (sigla) => {
+  return colecaoUf.filter(uf => uf.sigla.toLowerCase() === sigla.toLowerCase());
+};
